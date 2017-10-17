c37eb74d1a88966ee6ec4601232776f09a72b08e
davedumas0 <davedumas0@yahoo.com>
2017-10-16 20:38:48 -0700

    Update InternetFunctions.lua

--- InternetFunctions.lua
+++ InternetFunctions.lua
@@ -1,14 +1,12 @@
 
 --this function enables the mouse input for scaleform
 function enableMouse()
- PushScaleformMovieFunction(scaleform, "SET_MOUSE_INPUT")
- PushScaleformMovieFunctionParameterFloat(GetDisabledControlNormal(2, 239))
- PushScaleformMovieFunctionParameterFloat(GetDisabledControlNormal(2, 240))
- PushScaleformMovieFunctionParameterInt(-1)
- PopScaleformMovieFunctionVoid()
  if HasNamedScaleformMovieLoaded("web_browser") then
-   
- end 
+  PushScaleformMovieFunction(scaleform, "SET_MOUSE_INPUT")
+  PushScaleformMovieFunctionParameterFloat(GetDisabledControlNormal(2, 239))
+  PushScaleformMovieFunctionParameterFloat(GetDisabledControlNormal(2, 240))
+  PushScaleformMovieFunctionParameterInt(-1)
+  PopScaleformMovieFunctionVoid() 
  --IsControlJustPressed(2, 242)         
   if IsControlJustPressed(2, 242) then
    Citizen.Trace("is control 242 "..tostring(IsControlJustPressed(2, 241)).." bvar: "..tostring(bvar))
@@ -39,8 +37,18 @@ function enableMouse()
  PushScaleformMovieFunctionParameterFloat(bvar)
  PushScaleformMovieFunctionParameterBool(false)        			
  PopScaleformMovieFunctionVoid()
- 
+if IsControlJustPressed(1, 237) then
+ if HasNamedScaleformMovieLoaded("web_browser") then
+  PushScaleformMovieFunctionParameterFloat(16.0)
+  PushScaleformMovieFunction(scaleform, "SET_INPUT_EVENT")
+  PushScaleformMovieFunctionParameterFloat(16.0)
+  PopScaleformMovieFunctionVoid()  
+ else
+  PopScaleformMovieFunctionVoid()
+ end
+end 
 end
+end	
 ----------------------------------------------------
 ----------------------------------------------------
 
@@ -55,23 +63,4 @@ end
    getcurentwebsitPage = nil
    getcurentPage = nil
   end
-if IsControlJustPressed(1, 237) then
- if HasNamedScaleformMovieLoaded("web_browser") then
-  SetPedConfigFlag(PlayerPedId(), 185, true)
-PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
-PushScaleformMovieFunctionParameterInt(0)
-PushScaleformMovieFunctionParameterInt(2)
-PopScaleformMovieFunctionVoid()
-  PushScaleformMovieFunctionParameterFloat(16.0)
-  PushScaleformMovieFunction(scaleform, "SET_INPUT_EVENT")
-  PushScaleformMovieFunctionParameterFloat(16.0)
-  PopScaleformMovieFunctionVoid()  
- else
 
-  PopScaleformMovieFunctionVoid()
- end
-    
-else
- vVar = nil
- nvar = nil
-end
